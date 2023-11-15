c11b20ef849bb4413035b1067a232f94a9ca8cdd
Anton Martirez <82803657+AntonersGM@users.noreply.github.com>
2023-11-15 14:29:50 +0800

    Small design changes
    only header added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+// Card component to display individual posts
+const PostCard = ({ post }) => {
+  return (
+    <div className="post-card">
+      <h2>{post.title.rendered}</h2>
+      <div className="post-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+    </div>
+  );
+};
+
 function App() {
+  const adduLink = 'https://www.addu.edu.ph/wp-json/wp/v2/posts';
+  const postIDs = [13157, 13161, 13136];
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get('https://www.addu.edu.ph/wp-json/wp/v2/posts');
+        const response = await axios.get(adduLink);
         setData(response.data);
       } catch (error) {
         console.error('Failed to fetch data', error);
@@ -16,19 +29,20 @@ function App() {
     }
 
     fetchData();
-  }, []);
+  }, [postIDs]);
+
+  // Filter the data based on the specified postIDs
+  const filteredData = data.filter((value) => postIDs.includes(value.id));
 
   return (
-    <div className="App">
-      {}
-      <ul>
-        {data.map((value) => (
-          <li key={value.id}>
-            <h2>{value.title.rendered}</h2>
-            <div dangerouslySetInnerHTML={{ __html: value.content.rendered }} />
-          </li>
+    <div className=''>
+      <header className="bg-blue-500 Header1">The Official Website of AdDU Posts!</header>
+
+      <div className="App-header">
+        {filteredData.map((post) => (
+          <PostCard key={post.id} post={post} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
