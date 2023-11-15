77b477ba7cd68d0376bac4fb7a528f7c2ae06181
Anton Martirez <82803657+AntonersGM@users.noreply.github.com>
2023-11-15 22:17:43 +0800

    Changes
    Post date format below title, centered, and read more shows when it has more than 50 words

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import axios from 'axios';
 
 // Card component to display individual posts
 const PostCard = ({ post }) => {
+  // Parse the date string and format it
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  // Extract and format the date from the post
+  const postDate = formatDate(post.date);
+
+  // Limit the content to 100 words
+  const limitContent = (content, limit) => {
+    const words = content.split(' ');
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    }
+    return content;
+  };
+
+  const limitedContent = limitContent(post.content.rendered, 50);
+
   return (
     <div className="post-card">
       <div className="card">
         <h2 className="card-title">{post.title.rendered}</h2>
-        <div className="card-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        <p className="card-date">{postDate}</p>
+        <div className="card-content" dangerouslySetInnerHTML={{ __html: limitedContent }} />
+        {post.content.rendered.split(' ').length > 50 && (
+          <button className="read-more-button">Read More</button>
+        )}
       </div>
     </div>
   );
@@ -16,7 +40,7 @@ const PostCard = ({ post }) => {
 
 function App() {
   const adduLink = 'https://www.addu.edu.ph/wp-json/wp/v2/posts';
-  const postIDs = [13157, 13161, 13136];
+  const postIDs = [13157, 13161, 13136, 13147, 13126, 13102, 13090, 13171, 13060, 13034];
 
   const [data, setData] = useState([]);
 
