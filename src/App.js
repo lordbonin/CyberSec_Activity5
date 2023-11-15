d1e25687f800a17fef86db8c360f1c3600ed5870
lloydie <lbpogs07@gmail.com>
2023-11-15 21:00:26 +0800

    Cards

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import axios from 'axios';
 const PostCard = ({ post }) => {
   return (
     <div className="post-card">
-      <h2>{post.title.rendered}</h2>
-      <div className="post-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+      <div className="card">
+        <h2 className="card-title">{post.title.rendered}</h2>
+        <div className="card-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+      </div>
     </div>
   );
 };
@@ -35,8 +37,8 @@ function App() {
   const filteredData = data.filter((value) => postIDs.includes(value.id));
 
   return (
-    <div className=''>
-      <header className="bg-blue-500 Header1">The Official Website of AdDU Posts!</header>
+    <div className="App">
+      <header className="Header1">The Official Website of AdDU Posts!</header>
 
       <div className="App-header">
         {filteredData.map((post) => (
